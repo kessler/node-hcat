fb5b128e4a423ca2fa04499bbaee35936e6d2c60
Brian White <mscdex@mscdex.net>
2015-08-23 12:35:38 -0400

    Fix module name reference in usage text

--- usage.js
+++ usage.js
@@ -2,5 +2,5 @@ var table = require('text-table');
 
 module.exports = table([
 ['Options:\n'],
-['--port', 					'set a port for this bcat execution\n', 	'']
+['--port', 					'set a port for this hcat execution\n', 	'']
 ]);
